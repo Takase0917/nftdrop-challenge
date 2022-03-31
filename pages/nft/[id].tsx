f157b5e911319109bc80cbe2eca63910af578355
Takase0917 <takase0917@icloud.com>
2022-03-31 22:52:32 +0100

    chaged cat image

--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -17,7 +17,7 @@ function NFTDropPage() {
                 justify-center py-2 lg:min-h-screen'>
                     <div className='rounded-xl bg-gradient-to-br from-yellow-500 to-blue-900 p-2'>
                         <img className='w-44 rounded-xl 
-                    object-cover lg:h-96 lg:w-72' src="https://mintable-downloadable-payloads.s3.amazonaws.com/000000-0000000002/100598039386987804754287261408270175088215538607303956307490088068393024910867/81bfb151-c14e-4e73-8c25-409bc674a24d_Hunting.peg.jpeg?AWSAccessKeyId=ASIAUXGGXLIGYU2XLDP7&Expires=1648762567&Signature=4fpnmHvP6Gh%2Fia%2FSH4941f0YxTU%3D&x-amz-security-token=IQoJb3JpZ2luX2VjEIb%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEaCXVzLWVhc3QtMSJHMEUCIDFdrckKORc9AAOvYUQ43A22WDlL%2FpJNC2bHDdPn6I0RAiEAvZZjoMTrbngQpxUpXvu%2FUBZjBq34ZtOn5JLotDL1K1MqmAIIHhADGgwzMjQ2ODY3OTczMjUiDC4mQBubxnW5NFx94yr1AQWXwVxliP8hM0zFGEi1D%2BntNb78Hq2H%2BOAd7wfP2xFIkojj4S6cJHQrxKzXA5ZPQeyU7wVlEh6wA1hKq3rcGemWwpNJb2fhio0LJoqJXbR1uAjLo3Mq0UoUYrBWgOG8p6F%2Fv4BK08Gk3dKCa7VsVs%2F4XdBMRHiJT2wMutn1SBUHGadFrn3yxOW8zc6PA1OCtfSakXwlTavIC1DV48tWcFggI%2Br%2FIyQThiv3ef8c7uzCZXlqnZ9o1OfwmVD5eBr77yjQspDFzaVA6BFb1Ob2b8jtTVrxiIUmez1QBIcricWNwRWIitMa5%2Bu7CqbNx7uFy%2F4uSZQtML62mJIGOpoBVAivoAvZRu%2B2NFig0DUG81BuKuhYfJ94aUe1Kc%2F7OPp0aSlj0Yvu7kvnZvhja0pOuo8B59T3wr1p9vfOMGGkl1HimONBNU3IVUlGkBzcMLGjX61b4uJuca73ULXuNsXRCxoQX1bu3eSQu3aDw4IpdMMHdQZiD6WiL5R%2BneMIMZa9o%2Bkfz498dYuTN1r%2FJP8%2Fhexx7yYaWh9DCw%3D%3D" alt=""
+                    object-cover lg:h-96 lg:w-72' src="http://personal.rhul.ac.uk/zkac/332/cat.peg.jpeg" alt=""
                         />
 
                     </div>
