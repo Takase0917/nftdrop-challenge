8f413012eaa72d1d20484d0ce4b4c518cb7a940f
Takase0917 <takase0917@icloud.com>
2022-03-31 22:23:21 +0100

    Changed images

--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -11,22 +11,22 @@ function NFTDropPage() {
     return (
         <div className='flex h-screen flex-col lg:grid lg:grid-cols-10'>
             {/* left */}
-            <div className='lg:col-span-4 bg-gradient-to-br from-cyan-800
+            <div className='lg:col-span-4 bg-gradient-to-br from-black
             to-rose-500'>
                 <div className='flex flex-col items-center 
                 justify-center py-2 lg:min-h-screen'>
-                    <div className='rounded-xl bg-gradient-to-br from-yellow-400 to-purple-600 p-2'>
+                    <div className='rounded-xl bg-gradient-to-br from-yellow-500 to-blue-900 p-2'>
                         <img className='w-44 rounded-xl 
-                    object-cover lg:h-96 lg:w-72' src="https://links.papareact.com/8sg" alt=""
+                    object-cover lg:h-96 lg:w-72' src="https://mintable-downloadable-payloads.s3.amazonaws.com/000000-0000000002/100598039386987804754287261408270175088215538607303956307490088068393024910867/81bfb151-c14e-4e73-8c25-409bc674a24d_Hunting.peg.jpeg?AWSAccessKeyId=ASIAUXGGXLIGYU2XLDP7&Expires=1648762567&Signature=4fpnmHvP6Gh%2Fia%2FSH4941f0YxTU%3D&x-amz-security-token=IQoJb3JpZ2luX2VjEIb%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEaCXVzLWVhc3QtMSJHMEUCIDFdrckKORc9AAOvYUQ43A22WDlL%2FpJNC2bHDdPn6I0RAiEAvZZjoMTrbngQpxUpXvu%2FUBZjBq34ZtOn5JLotDL1K1MqmAIIHhADGgwzMjQ2ODY3OTczMjUiDC4mQBubxnW5NFx94yr1AQWXwVxliP8hM0zFGEi1D%2BntNb78Hq2H%2BOAd7wfP2xFIkojj4S6cJHQrxKzXA5ZPQeyU7wVlEh6wA1hKq3rcGemWwpNJb2fhio0LJoqJXbR1uAjLo3Mq0UoUYrBWgOG8p6F%2Fv4BK08Gk3dKCa7VsVs%2F4XdBMRHiJT2wMutn1SBUHGadFrn3yxOW8zc6PA1OCtfSakXwlTavIC1DV48tWcFggI%2Br%2FIyQThiv3ef8c7uzCZXlqnZ9o1OfwmVD5eBr77yjQspDFzaVA6BFb1Ob2b8jtTVrxiIUmez1QBIcricWNwRWIitMa5%2Bu7CqbNx7uFy%2F4uSZQtML62mJIGOpoBVAivoAvZRu%2B2NFig0DUG81BuKuhYfJ94aUe1Kc%2F7OPp0aSlj0Yvu7kvnZvhja0pOuo8B59T3wr1p9vfOMGGkl1HimONBNU3IVUlGkBzcMLGjX61b4uJuca73ULXuNsXRCxoQX1bu3eSQu3aDw4IpdMMHdQZiD6WiL5R%2BneMIMZa9o%2Bkfz498dYuTN1r%2FJP8%2Fhexx7yYaWh9DCw%3D%3D" alt=""
                         />
 
                     </div>
                     <div className='text-center p-5 space-y-2'>
                         <h1 className='text-4xl font-bold text-white'>
-                            PAPAFAM Apes
+                            Taka Apes
                         </h1>
                         <h2 className='text-xl text-gray-300'>
-                            A collection of PAPAFAM Apes who live & breathe React!
+                            A collection of Taka Apes who live & breathe React!
                         </h2>
                     </div>
                 </div>
@@ -41,7 +41,7 @@ function NFTDropPage() {
                         The{' '}
                         <span className='font-extrabold 
                         underline decoration-pink-600/50'>
-                            PAPAFAM
+                            TAKA
                         </span>{' '}
                         NFT market place
                     </h1>
@@ -63,9 +63,9 @@ function NFTDropPage() {
                 <div className='mt-10 flex flex-1 flex-col items-center space-y-6 
                  text-center lg:space-y-0 lg:justify-center'>
                     <img className='w-80 object-cover pb-10 
-                     lg:h-40' src="https://links.papareact.com/bdy" alt="" />
+                     lg:h-40' src="https://mintable-downloadable-payloads.s3.amazonaws.com/000000-0000000002/100598039386987804754287261408270175088215538607297340694940330150752136829205/81bfb151-c14e-4e73-8c25-409bc674a24d_theTwoBrothers.peg.jpeg?AWSAccessKeyId=ASIAUXGGXLIG2F25LMFJ&Expires=1648761692&Signature=CtRIYid33pljRQ8S%2B3ZIUcgf9fc%3D&x-amz-security-token=IQoJb3JpZ2luX2VjEIX%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEaCXVzLWVhc3QtMSJIMEYCIQCQ3m4xuW3CKggQdsZGeh4PC034XjfCvJOAFcOuYBsg1wIhAKxndEhGKJ7bS67II3UDyiJ6ZouSiL2PnZPeyxwnKEn9KpgCCB4QAxoMMzI0Njg2Nzk3MzI1Igw2X%2F%2FG%2F7bGPzAt2n8q9QGecILbqZuDE1vMUjlMGHrZVLqde1mzybqU71w1hdzatqHAIPZ5AXnSUeC5X%2FNMXslemDxTKtAg8yApVH2uLHlQpO8mpeeNOCdV0cYsVkpZw6vJbnbPLNQ0lKeHiIv2%2FkF3uCGd8UrTa6b0X3ozQm7tIRtBMlygH6afOxD2icAbQU9CZ69eSE5PfQatCw%2BLiOB%2BmlVs%2Flfw12dIUAr%2FceswnTtTDlPvExhJgZYfsMdLb6aqgUcDR7fcR%2BHrzb%2FcmA%2FWa8BJXZ67CkpYhq1QnpgHbmyd8NwqdwqzLZ98rkwfgxrHQ7VVhXAG%2BO0EKC8cCp8UJWVqjDDCrpiSBjqZAXP4CdCB6SapRDQ8getHa%2BpqebspMbVh2V8dmfOnEtYugGmuVy%2F2D3VNVpb0oPJPbnNkffM%2BGSjr55F%2FRKJIIwF6%2FGdQEEcizKAhj8xjuxDz6opr0A2Dap5BuQew1%2FBJcS3L%2FUk4jxKy00kvNBg9u1vOwqxBc3Trk2kynplTzWmfOlPisyRVgP08R3Ow1vvbmuOlsUpxi50hBw%3D%3D" alt="" />
                     <h1 className='text-3xl font-bold lg:text-5xl lg:font-extrabold'>
-                        The papafam ape coding club
+                        The TAKA ape coding club
                     </h1>
                     <p className='pt-2 text-xl text-green-500'>13/21 NFT's claimed</p>
                 </div>
